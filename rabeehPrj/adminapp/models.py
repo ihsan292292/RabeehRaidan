a0143f203266e317ae29e37eb3bcd64ef27c01a8
skillBoardEdu <skillboardeducationpandikkad@gmail.com>
2024-05-29 16:19:25 +0530

    oreder details added

--- rabeehPrj/adminapp/models.py
+++ rabeehPrj/adminapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser,Group,Permission
 # Create your models here.
+
+
+    
+class Branch(models.Model):
+    location = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    gmap = models.URLField(max_length=200,blank=True,null=True)
+    def __str__(self):
+        return self.location
+    
+
+class Staff(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    phone = models.CharField(max_length=50,null=True,blank=True)
+    branch = models.ForeignKey(Branch,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username 
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    phone = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.user.username
+    
+class Order(models.Model):
+    user = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    item = models.ForeignKey(Product,on_delete=models.CASCADE)
+    price = models.IntegerField()
+    location = models.ForeignKey(Branch,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.item
--- rabeehPrj/adminapp/urls.py
+++ rabeehPrj/adminapp/urls.py
@@ -6,10 +6,12 @@ router = routers.SimpleRouter()
 
 urlpatterns = [
     path('', custom_login_view, name='custom_admin_login'),
-    path('home/', home_view, name='home'),
+    path('logout/',custom_logout_view,name="custom_logout_view"),
+    path('home/', home_view, name='admin_home'),
     path('forms/',forms, name='forms'),
     path('table/',tables, name="tables"),
     path('product/',product, name="product"),
     path('staff/',staff, name="staff"), 
     path('branches/',branch, name="branch"),
+    path('offers/',offers, name="offers")
 ]
